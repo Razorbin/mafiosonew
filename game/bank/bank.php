b06f8b041872606c4ad826a5657c09e6a94fc879
lars fredrik holst-try <lars.fr.97@hotmail.com>
2023-06-14 16:06:59 +0200

    bank wip

--- game/bank/bank.php
+++ game/bank/bank.php
@@ -1,1 +1,95 @@
-jeg er bank
+<div class="functionContainer df g5">
+  <div class="gameBox fb50 df aic">
+    <div class="df g5 fdcol w-100">
+      <div class="df aic g5 jcsb w-100">
+        <p class="textSecondary">Din saldo</p>
+        <h3>100 000 000<span class="textSecondary fontSmall"> ,-</span></h3>
+      </div>
+      <div class="df aic g5 jcsb">
+        <p class="textSecondary">Totalt overført/mottatt</p>
+        <h3>1 000<span class="textSecondary fontSmall"> ,- /</span> 5 000<span class="textSecondary fontSmall"> ,-</span></h3>
+      </div>
+      <div class="df aic g5 jcsb">
+        <p class="textSecondary">Renter ved midnatt</p>
+        <h3>10 000 000 <span class="textSecondary fontSmall">(10%)</span></h3>
+      </div>
+    </div>
+  </div>
+  <div class="gameBox fb50">
+    <div class="df g5 fdcol">
+      <div>
+        <input class="textInput w-100" type="text" placeholder="Sum">
+      </div>
+      <div class="df g5 aic">
+        <input class="fg1" type="submit" value="Sett inn">
+        <input class="fg1" type="submit" value="Ta ut">
+        <input class="fg1" type="submit" value="Sett inn alt">
+        <input class="fg1" type="submit" value="Ta ut alt">
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="functionContainer df g5">
+  <div class="gameBox fb50">
+    <div class="df jcc">
+      <h3>Siste 10 overførsler</h3>
+    </div>
+
+    <div class="transfer df jcsb aic">
+      <div class="df aic g5">
+        <div class="avatar"></div>
+        <div class="userInfo">
+          <h3>Skitzo</h3>
+          <p>4 juni 2023</p>
+        </div>
+      </div>
+      <div class="df">
+        <h3 class="lime">+ 4 500 000,-</h3>
+      </div>
+    </div>
+
+    <div class="transfer df jcsb aic">
+      <div class="df aic g5">
+        <div class="avatar"></div>
+        <div class="userInfo">
+          <h3>Skitzo</h3>
+          <p>4 juni 2023</p>
+        </div>
+      </div>
+      <div class="df">
+        <h3 class="lime">+ 4 500 000,-</h3>
+      </div>
+    </div>
+
+    <div class="transfer df jcsb aic">
+      <div class="df aic g5">
+        <div class="avatar"></div>
+        <div class="userInfo">
+          <h3>Skitzo</h3>
+          <p>4 juni 2023</p>
+        </div>
+      </div>
+      <div class="df">
+        <h3 class="orange">- 4 500 000,-</h3>
+      </div>
+    </div>
+
+  </div>
+  <div class="gameBox fb50"></div>
+</div>
+
+<style>
+  .transfer:not(:first-child) {
+    margin-top: 10px;
+    padding: 5px;
+    border-top: 1px solid #242424;
+  }
+
+  .transfer .avatar {
+    height: 25px;
+    width: 25px;
+    border-radius: 20px;
+    background-color: orange;
+  }
+</style>
--- index.php
+++ index.php
@@ -7,6 +7,7 @@ include 'helpers.php';
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -17,51 +18,39 @@ include 'helpers.php';
     <link rel="icon" type="image/x-icon" href="media/favicon-32x32.png">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
+
 <body>
     <div class="main">
-        <?php 
-        
+        <?php
+
         include 'content/header.php';
         include 'content/underHeader.php';
-        
+
         ?>
 
         <div class="container">
             <?php include 'content/actionHeader.php' ?>
-    
+
             <div class="content">
                 <?php include 'content/leftMenu.php' ?>
                 <div class="game">
-                   <?php 
-                   
-                   include 'content/gameHeader.php';
-                   ?>
+                    <?php
 
-                <div id="gameContent">
+                    include 'content/gameHeader.php';
+                    ?>
 
-                </div>
-
-                   <?php
-
-                    // if(isset($_GET['page'])){
-                    //     if(!is_dir('game/'.$_GET['page'])){
-                    //         echo '404';
-                    //     } else {
-                    //         include 'game/'.$_GET['page'].'/'.$_GET['page'].'.php';
-                    //     }
-                    // }
-                   
-                   ?>
+                    <div id="gameContent"></div>
                 </div>
                 <?php include 'content/rightMenu.php' ?>
-                <button onclick="newSnackbar('test', 'success')">Show Snackbar</button>
+                <!-- <button onclick="newSnackbar('test', 'success')">Show Snackbar</button> -->
             </div>
         </div>
     </div>
 </body>
+
 </html>
 
-  <script src="js/snackbar.js"></script>
-  <script src="js/router.js"></script>
-  <script src="js/getData.js"></script>
-  <script src="js/date.js"></script>
+<script src="js/snackbar.js"></script>
+<script src="js/router.js"></script>
+<script src="js/getData.js"></script>
+<script src="js/date.js"></script>
