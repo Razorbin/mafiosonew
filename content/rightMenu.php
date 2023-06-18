3f4840d5810599f0426e8aaf7832adcb595f32f1
Lars Fredrik Holst-Try <lars.fr.97@hotmail.com>
2023-06-18 17:42:19 +0200

    smaller username

--- content/rightMenu.php
+++ content/rightMenu.php
@@ -1,7 +1,7 @@
 <div class="rightMenu">
     <div class="df g5 fdcol">
         <div class="df userInfo aic jcsb">
-            <h4>Sktitzo</h4>
+            <h5>Sktitzo</h5>
             <span class="textSecondary">Capo Di Tutti Frutti</span>
         </div>
         <div class="rankbar">
