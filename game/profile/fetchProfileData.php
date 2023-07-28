ef63c90b0fc8a38db4f0ed7ac5a7cf8757f82320
Lars Fredrik Holst-Try <lars.fr.97@hotmail.com>
2023-07-28 11:04:35 +0200

    get rank

--- game/profile/fetchProfileData.php
+++ game/profile/fetchProfileData.php
@@ -1,17 +1,18 @@
 <?php
 
 include '../../db/db.php';
+include '../../functions/ranks.php';
 
 // Get the 'user' parameter from the URL
 $user = $_GET['user'];
 
-$stmt = $pdo->prepare('SELECT username FROM user WHERE username = :username');
+$stmt = $pdo->prepare('SELECT username, exp FROM user WHERE username = :username');
 $stmt->execute(['username' => $user]);
 $user_row = $stmt->fetch();
 
 $userData['username'] = $user_row['username'];
 $userData['status'] = 'Active';
-$userData['rank'] = 'Gangster';
+$userData['rank'] = get_rank($user_row['exp']);
 $userData['pengerank'] = 'High Roller';
 $userData['drap'] = '10';
 $userData['familie'] = 'Mafia Family';
